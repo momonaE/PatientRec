f39bf7eb19610d0209a2a56e64083a10f3b34af6
unknown <mberhane@miu.edu>
2022-01-27 23:16:17 -0600

    Physician added

---
+++ Physician/src/test/java/com/MedicalRecord/Physician/PhysicianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedicalRecord.Physician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
